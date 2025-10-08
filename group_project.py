b106948a563eb00922fbd5c1492067fcb37c3262
birgit <birgitaaschouten@hotmail.com>
2025-10-08 22:19:39 +0200

    Working on visualisation based on community

--- group_project.py
+++ group_project.py
@@ -31,5 +31,17 @@ with open('edgelist-late.csv', 'r') as edgefile:
         node1, node2, weight = line.split(',')
         graph_late.add_edge(node1, node2, weight = weight)
 
-nx.draw_networkx(graph_early, pos=nx.spring_layout(graph_early), with_labels = False)
+get_community = nx.get_node_attributes(graph_early, 'community')
+node_colors = []
+for node in graph_early:
+    print(node, get_community[node])
+    if get_community[node] == 'Professional':
+        node_colors.append('red')
+    elif get_community[node] == "Personal":
+        node_colors.append('blue')
+    else:
+        node_colors.append('purle')
+print(node_colors)
+
+nx.draw_networkx(graph_early, pos=nx.spring_layout(graph_early), with_labels = False, node_size = 100)
 plt.savefig('graph_early.png')
